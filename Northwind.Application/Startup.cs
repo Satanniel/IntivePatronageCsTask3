b9866073067d75bd9b820fb6b12a8699f51a5c81
satanniel@gmail.com <satanniel@gmail.com>
2019-01-29 23:48:44 +0100

    update

--- Northwind.Application/Startup.cs
+++ Northwind.Application/Startup.cs
@@ -22,6 +22,7 @@ namespace Northwind.Application
             {
                 c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
             });
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
